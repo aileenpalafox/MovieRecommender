fdea9d3b43949e775470e5d70edbed2bcf0895dd
Aileen <aileenpalafox2012@gmail.com>
2021-04-26 15:12:12 -0700

    data model added

--- src/main/java/movie/recommender/MovieRecommender.java
+++ src/main/java/movie/recommender/MovieRecommender.java
@@ -1,60 +1,128 @@
 package movie.recommender;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.zip.GZIPInputStream;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class MovieRecommender {
     private int totalReviews = 0;
     private int totalProducts = 0;
     private int totalUsers = 0;
-    ArrayList<String> productsIds = new ArrayList<String>();
-    ArrayList<String> usersIds = new ArrayList<String>();
+    private int idproduct = 0;
+    private int iduser = 0;
+    private String idproductline = null;
+    private String iduserline = null;
+    private String score;
+    private List<String> RecommendationsForUser;
+    Hashtable<String, Integer> productsIds = new Hashtable<String, Integer>();
+    Hashtable<String, Integer> usersIds = new Hashtable<String, Integer>();
 
     public MovieRecommender(String path) throws IOException {
         //decodes gzip file
         InputStream gzipStream = new GZIPInputStream(new FileInputStream(path)); //InputStream fileStream = new FileInputStream(filename);
         BufferedReader buffered = new BufferedReader(new InputStreamReader(gzipStream)); //Reader decoder = new InputStreamReader(gzipStream, encoding)
         String readingLine;
-        String productId; //mas descriptivo
-        String userId;
+        FileWriter writer = new FileWriter("/Users/aileen.palafox/Documents/movies.csv");
 
-        while ((readingLine=buffered.readLine())!=null){ //
+        while ((readingLine = buffered.readLine()) != null) { //
             //uses the word "product" as identifier for the beginning of each review
-           if(readingLine.startsWith("product")){
-               this.totalReviews++;
+            if (readingLine.startsWith("product/productId")) {
+                totalReviews++;
+                System.out.println(totalReviews);
+                idproductline = readingLine.split(" ")[1];
+                //stores unique products ids in arraylist
+                if (!productsIds.contains(idproductline)) {
+                    totalProducts++;
+                    productsIds.put(idproductline, totalProducts);
+                    idproduct = totalProducts;
+                } else {
+                    idproduct = productsIds.get(idproductline);
+                }
+            }
+            if (readingLine.startsWith("review/userId")) {
+                iduserline = readingLine.split(" ")[1];
+                //stores unique users ids in arraylist
+                if (!usersIds.contains(iduserline)) {
+                    totalUsers++;
+                    usersIds.put(iduserline, totalUsers);
+                    iduser = totalUsers;
+                } else {
+                    iduser = usersIds.get(iduserline);
+                }
+            }
+            if (readingLine.startsWith("review/score")) {
+                score = readingLine.split(" ")[1];
+                writer.write(iduser + "," + idproduct + "," + score + "\n");
+            }
 
-              //stores unique products ids in arraylist
-               productId = readingLine.split(" ")[1];
-               if (!this.productsIds.contains(productId)){
-                   this.productsIds.add(productId);
-               }
-           }
-
-           if(readingLine.startsWith("review/userId")){
-               //stores unique users ids in arraylist
-               userId = readingLine.split(" ")[1];
-               if (!usersIds.contains(userId)){
-                   usersIds.add(userId);
-               }
-           }
         }
+        buffered.close();
+        gzipStream.close();
+        writer.close();
 
-        System.out.println("total reviews "+totalReviews);
-        System.out.println("total products "+productsIds.size());
-        System.out.println("total users "+usersIds.size());
+        System.out.println("total reviews " + totalReviews);
+        System.out.println("total products " + productsIds.size());
+        System.out.println("total users " + usersIds.size());
     }
 
+    /**
+     * Based on the given files count the total of reviews by the users.
+     * @return Total reviews
+     */
     public int getTotalReviews() {
-        return this.totalReviews;
+        return totalReviews;
     }
 
+    /**
+     * Based on the given file counts the number of different products
+     * @return Total products
+     */
     public int getTotalProducts() {
-        return this.totalProducts=productsIds.size();
+        return totalProducts;
     }
 
+    /**
+     * Based on the given file counts the number of different users
+     * @return Total users
+     */
     public int getTotalUsers() {
-        return totalUsers=usersIds.size();
+        return totalUsers;
+    }
+
+    /**
+     * Given an user returns 3 movies recommendations based on they similarity with other users
+     * @param userId
+     * @return A list of recommendations for the user
+     */
+    public List<String> getRecommendationsForUser(String userId) {
+        try {
+            int key = usersIds.get(userId);
+            DataModel model = new FileDataModel(new File("/Users/aileen.palafox/Documents/movies.csv"));
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+            UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+            List<RecommendedItem> recommendations = recommender.recommend(key, 3);
+            for (RecommendedItem recommendation : recommendations) {
+                RecommendationsForUser.add(recommendation.toString());
+                System.out.println("recommendations " + recommendation);
+            }
+        } catch (TasteException | IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        System.out.println("Recommendations " + RecommendationsForUser);
+        return RecommendationsForUser;
     }
 
 }
--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
@@ -5,12 +5,12 @@ import org.apache.mahout.cf.taste.common.TasteException;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.hasItem;
 
-//importar clase con el codigo funcional, es necesario tenerla dentro de un package para que Maven la detecte
 import movie.recommender.MovieRecommender;
 import movie.recommender.ToCSV;
 
@@ -22,19 +22,19 @@ public class MovieRecommenderTest {
         //download moviestest.gz from
         //    http://snap.stanford.edu/data/web-Movies.html
 
-        //MovieRecommender recommender = new MovieRecommender("/Users/aileen.palafox/Documents/movies.txt.gz");
-        //MovieRecommender recommender = new MovieRecommender("/Users/aileen.palafox/Documents/academy-exercises/src/main/resources/moviestest2.txt.gz");
-        //assertEquals(7911684, recommender.getTotalReviews());
-        //assertEquals(253059, recommender.getTotalProducts());
-        //assertEquals(889176, recommender.getTotalUsers());
+        MovieRecommender recommender = new MovieRecommender("/Users/aileen.palafox/Documents/movies.txt.gz");
+        //MovieRecommender recommender = new MovieRecommender("/Users/aileen.palafox/Documents/academy-exercises/src/main/resources/movies50k.txt.gz");
+        assertEquals(7911684, recommender.getTotalReviews());
+        assertEquals(253059, recommender.getTotalProducts());
+        assertEquals(889176, recommender.getTotalUsers());
 
-        ToCSV tocsv = new ToCSV("/Users/aileen.palafox/Documents/academy-exercises/src/main/resources/moviestest2.txt.gz");
+        //ToCSV tocsv = new ToCSV("/Users/aileen.palafox/Documents/academy-exercises/src/main/resources/movies50k.txt.gz");
         //extract extractMovies = new extract("/Users/aileen.palafox/Documents/movies.txt.gz");
 
-        //List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR"); //usar mahout
-        //assertThat(recommendations, hasItem("B0002O7Y8U")); //dejar comentado
-        //assertThat(recommendations, hasItem("B00004CQTF"));
-        //assertThat(recommendations, hasItem("B000063W82"));
+        List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");//A141HP4LYPWMSR
+        assertThat(recommendations, hasItem("B0002O7Y8U")); //dejar comentado
+        assertThat(recommendations, hasItem("B00004CQTF"));
+        assertThat(recommendations, hasItem("B000063W82"));
 
     }
 
