c7e60cb19bd08a3487f1cc55b8553a2ee5921bd6
Aileen <aileenpalafox2012@gmail.com>
2021-04-26 20:27:01 -0700

    comment code deleted

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommenderTest.java
@@ -1,6 +1,5 @@
 package nearsoft.academy.bigdata.recommendation;
 
-import movie.recommender.extract;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.junit.Test;
 
@@ -10,11 +9,8 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.hasItem;
-
 import movie.recommender.MovieRecommender;
-import movie.recommender.ToCSV;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 public class MovieRecommenderTest {
     @Test
@@ -23,15 +19,11 @@ public class MovieRecommenderTest {
         //    http://snap.stanford.edu/data/web-Movies.html
 
         MovieRecommender recommender = new MovieRecommender("/Users/aileen.palafox/Documents/movies.txt.gz");
-        //MovieRecommender recommender = new MovieRecommender("/Users/aileen.palafox/Documents/academy-exercises/src/main/resources/movies50k.txt.gz");
         assertEquals(7911684, recommender.getTotalReviews());
         assertEquals(253059, recommender.getTotalProducts());
         assertEquals(889176, recommender.getTotalUsers());
 
-        //ToCSV tocsv = new ToCSV("/Users/aileen.palafox/Documents/academy-exercises/src/main/resources/movies50k.txt.gz");
-        //extract extractMovies = new extract("/Users/aileen.palafox/Documents/movies.txt.gz");
-
-        List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");//A141HP4LYPWMSR
+        List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");
         assertThat(recommendations, hasItem("B0002O7Y8U")); //dejar comentado
         assertThat(recommendations, hasItem("B00004CQTF"));
         assertThat(recommendations, hasItem("B000063W82"));
